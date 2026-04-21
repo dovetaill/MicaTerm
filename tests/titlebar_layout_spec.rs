ada976e459630eb78d453eef0b4b42f70148896b
Qi <7siyun@gmail.com>
2026-04-21 15:30:25 +0800

    feat: add shell focus mode

--- tests/titlebar_layout_spec.rs
+++ tests/titlebar_layout_spec.rs
@@ -9,7 +9,7 @@ fn top_status_bar_layout_matches_sync_first_class_action_budget() {
     assert_eq!(ShellMetrics::TITLEBAR_BRAND_WIDTH, 188);
     assert_eq!(ShellMetrics::TITLEBAR_TOOL_BUTTON_SIZE, 36);
     assert_eq!(ShellMetrics::TITLEBAR_TOOL_ICON_SIZE, 20);
-    assert_eq!(ShellMetrics::TITLEBAR_UTILITY_WIDTH, 220);
+    assert_eq!(ShellMetrics::TITLEBAR_UTILITY_WIDTH, 260);
     assert_eq!(ShellMetrics::TITLEBAR_WINDOW_CONTROL_WIDTH, 138);
 }
 
